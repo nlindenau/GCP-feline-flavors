649bea4d0af338ad3f22d9762ebb22ae7c526f2c
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2023-12-05 18:12:44 +0200

    feat: add barebones tf configuration

---
+++ ep-01-static-page/terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "project" {
+  type        = string
+  description = "This is your unique project ID in GCP. You can find it in GCP console."
+  #If you do not want to enter your project ID when running terraform operations, you could pass the value as default.
+  #default=""
+}
+
+# You can find region and zones here: https://cloud.google.com/compute/docs/regions-zones
+variable "region" {
+  type        = string
+  description = "Region is a geography, in which your resources will be created. Choose one that is close to where you are in the world."
+  #If you do not want to enter your region when running terraform operations, you could pass the value as default.
+  #default=""
+}
+variable "zone" {
+  type        = string
+  description = "Zone is a smaller division within region. Some resources are zonal, as opposed to just regional or global."
+  #If you do not want to enter your zone when running terraform operations, you could pass the value as default.
+  #default=""
+}
